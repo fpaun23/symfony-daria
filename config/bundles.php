94ab4ad7ef6bc5c948ddac75949dd2fe1276519d
daria0805 <dariabobitan@yahoo.com>
2022-12-02 14:10:42 +0200

    commit task6 CRUD

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,5 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
 ];
--- src/Controller/CompanyController.php
+++ src/Controller/CompanyController.php
@@ -1,7 +1,5 @@
 <?php
 
-declare (strict_types = 1);
-
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -11,6 +9,10 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\HttpFoundation\Request;
 use Psr\Log\LoggerInterface;
+use App\Entity\Company;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
 
 /**
  * class for company controller
@@ -18,8 +20,8 @@ use Psr\Log\LoggerInterface;
 class CompanyController extends AbstractController
 {
     private $logger;
-    
-    
+
+
     /**
      * __construct
      *
@@ -30,49 +32,117 @@ class CompanyController extends AbstractController
     {
         $this->logger = $logger;
     }
-    
+
     /**
      * loadTemplate
      *
      * @return Response
      */
-    public function loadTemplate():Response
+    public function loadTemplate(): Response
     {
-        return $this->render('company/index.html.twig', ['name'=>'Welcome']);
+        return $this->render('company/index.html.twig', ['name' => 'Welcome']);
     }
-    
+
     /**
      * add fields for the form and the submitted data in varlog
      *
      * @param  mixed $request
      * @return Response
      */
-    public function add(Request $request):Response
+    // public function add(Request $request): Response
+    // {
+    //     $this->form = $this->createFormBuilder()
+    //         ->add('name', TextType::class)
+    //         ->add('description', TextType::class)
+    //         ->add('email', EmailType::class)
+    //         ->add('saveData', SubmitType::class, ['label' => 'Create Company'])
+    //         ->getForm();
+
+    //     $this->form->handleRequest($request);
+
+    //     if ($this->form->isSubmitted() && $this->form->isValid()) {
+    //         $data = $this->form->getData();
+    //         $name = $data['name'];
+    //         $description = $data['description'];
+    //         $email = $data['email'];
+
+
+    //         $this->logger->error(
+    //             "Data is saved!",
+    //             [json_encode(['name' => $name, 'description' => $description , 'email' => $email])]
+    //         );
+    //         return $this->redirectToRoute('company_controller');
+    //     }
+    //     return $this->render('company/index.html.twig', [
+    //         'form' => $this->form->createView()
+    //     ]);
+    // }
+    /**
+     * adds Company to the db
+     *
+     * @param  mixed $doctrine
+     * @return Response
+     */
+    public function addCompany(ManagerRegistry $doctrine): Response
     {
-        $this->form = $this->createFormBuilder()
-            ->add('name', TextType::class)
-            ->add('description', TextType::class)
-            ->add('email', EmailType::class)
-            ->add('saveData', SubmitType::class, ['label' => 'Create Company'])
-            ->getForm();
+        $entityManager = $doctrine->getManager();
 
-        $this->form->handleRequest($request);
+        $company = new Company();
+        $company->setName('ApacheNe2');
+        $entityManager->persist($company);
+        $entityManager->flush();
 
-        if ($this->form->isSubmitted() && $this->form->isValid()) {
-            $data=$this->form->getData();
-            $name=$data['name'];
-            $description= $data['description'];
-            $email= $data['email'];
-
-
-            $this->logger->error(
-                "Data is saved!",
-                [json_encode(['name' => $name, 'description' => $description,'email'=>$email])]
-            );
-            return $this->redirectToRoute('company_controller');
+        return new Response('Saved new company with id ' . $company->getId());
+    }
+    /**
+     * updates company name with id
+     *
+     * @param  mixed $doctrine
+     * @param  mixed $id
+     * @return Response
+     */
+    public function updateCompany(ManagerRegistry $doctrine, int $id): Response
+    {
+        $updateId = null;
+        $entityManager = $doctrine->getManager();
+        $company = $entityManager->getRepository(Company::class)->find($id);
+        if (!empty($company)) {
+            $updateId = $company->getId();
+            $company->setName('.NetDev');
+            $entityManager->flush();
         }
-        return $this->render('company/index.html.twig', [
-            'form'=>$this->form->createView()
+        return new JsonResponse([
+            'update' => !empty($updateId),
+            'updateId' => $updateId
         ]);
+    }
+    public function deleteCompany(ManagerRegistry $doctrine, int $id): Response
+    {
+        $deletedId = null;
+        $entityManager = $doctrine->getManager();
+        $company = $entityManager->getRepository(Company::class)->find($id);
+        if (!empty($company)) {
+            $deletedId = $company->getId();
+            $entityManager->remove($company);
+            $entityManager->flush();
+        }
+
+        return new JsonResponse([
+            'detleted' => !empty($deletedId),
+            'deleteId' => $deletedId
+        ]);
+    }
+    /**
+     * lists the companies from the db
+     *
+     * @param  mixed $doctrine
+     * @param  mixed $id
+     * @return Response
+     */
+    public function listCompany(ManagerRegistry $doctrine): Response
+    {
+        $company = $doctrine->getRepository(Company::class)->findAll();
+        //var_dump($company);die;
+        return new Response(json_encode($company));
     }
 }
